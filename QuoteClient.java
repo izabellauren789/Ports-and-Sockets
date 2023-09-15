ebfa78ac990c221419a16ca046efbef23c02aed4
euilee0129 <superbankaisam@gmail.com>
2023-09-15 15:04:58 -0700

    update?
    ahhhhhhhhhhhhhhhhhhhh

--- QuoteClient.java
+++ QuoteClient.java
@@ -9,4 +9,24 @@ import java.io.*;
 public class QuoteClient
 {
 	//Your code is here
+	public static void main(String[] args) {
+		try 
+		{
+			/* make connection to server socket */
+			Socket sock = new Socket("127.0.0.1",6017);
+			InputStream in = sock.getInputStream();
+			BufferedReader bin = new
+			BufferedReader(new InputStreamReader(in));
+			/* read the date from the socket */ //read quote of day from server
+			String line;
+			while ( (line = bin.readLine()) != null)
+			System.out.println(line);
+			/* close the socket connection*/
+		sock.close();
+		}
+		catch (IOException ioe) 
+		{
+			System.err.println(ioe);
+		}
+	}
 }
--- QuoteServer.java
+++ QuoteServer.java
@@ -9,4 +9,27 @@ import java.io.*;
 public class QuoteServer
 {
   // Your code is here
+  public static void main(String[] args) {
+    try 
+    {
+      ServerSocket sock = new ServerSocket(6017); // change this to port 17, where we pull from pre existing
+      /* now listen for connections */
+      while (true) 
+      {
+        Socket client = sock.accept();
+        PrintWriter pout = new
+        PrintWriter(client.getOutputStream(), true);
+        /* write the Date to the socket */ //write quote of day
+        /*CHANGE THIS LINE */
+        pout.println(new java.util.Date().toString()); //change front part, keep toString()
+        /* close the socket and resume */
+        /* listening for connections */
+        client.close();
+      }
+    }
+    catch (IOException ioe) 
+    {
+      System.err.println(ioe);
+    }
+  }
 }
