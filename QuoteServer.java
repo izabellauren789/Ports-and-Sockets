8f4eaeb6b3a80691ea54275275438d2c4345230e
euilee0129 <superbankaisam@gmail.com>
2023-09-15 15:19:07 -0700

    Update QuoteServer.java

--- QuoteServer.java
+++ QuoteServer.java
@@ -11,16 +11,16 @@ public class QuoteServer
   public static void main(String[] args) {
     try 
     {
-      ServerSocket sock = new ServerSocket(17);
+      ServerSocket sock = new ServerSocket(17); //pulling from pre existing port
       /* now listen for connections */
       while (true) 
       {
         Socket client = sock.accept();
         PrintWriter pout = new
         PrintWriter(client.getOutputStream(), true);
-        /* write the Date to the socket */ 
+        /* write the Date to the socket */ //change to quote of the day
         /*CHANGE THIS LINE */
-        pout.println(new java.util.Date().toString());
+        pout.println(new java.util.Date().toString()); // change front part, keep toString()
         /* close the socket and resume */
         /* listening for connections */
         client.close();
