2dc0f696dc7726eebeafaf26466b1dd674a7e686
izabellauren789 <izabellauren789@gmail.com>
2023-09-18 17:32:15 -0700

    adding code

--- EchoClient.java
+++ EchoClient.java
@@ -2,10 +2,34 @@
  * An echo client. The client enters data to the server, and the
  * server echoes the data back to the client.
  */
-import java.net.*;
 import java.io.*;
+import java.net.*;
 
-public class EchoClient
-{
-	//Your code is here
+public class EchoClient {
+    public static void main(String[] args) 
+    {
+        String serverHost = "127.0.0.1";
+        int serverPort = 6007;
+
+        try (Socket socket = new Socket(serverHost, serverPort)) 
+        {
+            OutputStream outputStream = socket.getOutputStream();
+            InputStream inputStream = socket.getInputStream();
+
+            // Send data to the server
+            String message = "Hello, world";
+            outputStream.write(message.getBytes());
+
+            // Receive and print the response from the server
+            byte[] buffer = new byte[1024];
+            int bytesRead = inputStream.read(buffer);
+            if (bytesRead != -1) {
+                String response = new String(buffer, 0, bytesRead);
+                System.out.println("Received: " + response);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
--- EchoServer.java
+++ EchoServer.java
@@ -3,10 +3,33 @@
  * and echoes back the result. 
  */
 
-import java.net.*;
-import java.io.*;
-
-public class  EchoServer
-{
- //Your code is here
-}
+ import java.io.*;
+ import java.net.*;
+ 
+ public class EchoServer {
+     public static void main(String[] args) {
+         int port = 6007;
+         try (ServerSocket serverSocket = new ServerSocket(port)) {
+             System.out.println("Server listening on port " + port);
+             
+             while (true) {
+                 try (Socket clientSocket = serverSocket.accept()) {
+                     InputStream inputStream = clientSocket.getInputStream();
+                     OutputStream outputStream = clientSocket.getOutputStream();
+                     
+                     byte[] buffer = new byte[1024];
+                     int bytesRead;
+                     
+                     while ((bytesRead = inputStream.read(buffer)) != -1) {
+                         outputStream.write(buffer, 0, bytesRead);
+                     }
+                 } catch (IOException e) {
+                     e.printStackTrace();
+                 }
+             }
+         } catch (IOException e) {
+             e.printStackTrace();
+         }
+     }
+ }
+ 
