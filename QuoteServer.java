a93a9471f627ff7fefd7028e4f3e02eb339616a1
izabellauren789 <izabellauren789@gmail.com>
2023-09-18 17:20:02 -0700

    troubleshooting

--- QuoteServer.java
+++ QuoteServer.java
@@ -13,21 +13,23 @@
        /* now listen for connections */
        while (true) {
          Socket client = sock.accept();
-         PrintWriter pout = new PrintWriter(client.getOutputStream(), true);
+         PrintWriter pout = new PrintWriter(client.getOutputStream(),true);
+
  
          // Create a Socket to connect to the target server on port 17 (Quote of the day)
-         Socket targetServer = new Socket("127.0.0.1", 17); // You can replace "localhost" with the actual server's IP
-         
+         Socket targetServer = new Socket("127.0.0.1", 17);
          // Get input and output streams for communication with the target server
          InputStream targetIn = targetServer.getInputStream();
          BufferedReader targetReader = new BufferedReader(new InputStreamReader(targetIn));
  
          // Read the quote of the day from the target server
          String quoteLine;
-         while ((quoteLine = targetReader.readLine()) != null) {
+         while ((quoteLine = targetReader.readLine()) != null) 
+         {
            pout.println(quoteLine);
          }
  
+        //pout.println(new java.util.Date().toString());
          // Close the sockets
          client.close();
          targetServer.close();
